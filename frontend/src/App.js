c7c39a4e1227e0e27a3954ae79174659025b9552
ashdaily <ashishsinghbardhan@gmail.com>
2020-11-28 20:21:31 +0900

    add sass, remove app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,13 @@ import {
   Route
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
+import "./App.scss";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Pay from "./pages/Pay";
 import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Switch>
             <PrivateRoute exact path="/pay" component={Pay} />
             <Route exact path="/" component={ProductList} />
+            <Route exact path="/:id" component={ProductDetails} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
           </Switch>
