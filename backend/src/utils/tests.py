bda4fd107279a5365fa31fae66047a5ac5eb1080
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-02-27 05:26:41 +0900

    issue#5 pagination api test cases

---
+++ backend/src/utils/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+import json
+
+
+class TestCaseBase(TestCase):
+    def make_random_model_data(self, model, count: int, *none_fields):
+        o = model.objects.get(id=1)
+        objects = []
+        for _ in range(count):
+            o.id = None
+            for none_field in none_fields:
+                setattr(o, none_field, None)
+            objects.append(o)
+
+        model.objects.bulk_create(objects)
+
+    def deserialize(self, payload: str) -> list or dict:
+        return json.loads(payload)
