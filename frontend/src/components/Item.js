2c67b5ecbd16c9da20f69edb487c8a5cd1b05eda
jitesh goel <jitesh.goel@imarkinfotech.com>
2021-03-19 19:41:22 +0530

    issue#33: solved Frontend bug: Out of stock products should not have clickable qty

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -2,13 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 export default ({ product, outOfStock }) => {
-	const [stockText, setoutOfStock] = useState('')
+	const [stockText, setoutOfStock] = useState("");
 
 	const { removeProduct, increase, decrease } = useContext(ShopContext);
 
 	const handleRemove = (id, variant_id, size) => {
 		removeProduct(id, variant_id, size);
-	}
+	};
 
 	useEffect(() => {
 		if (outOfStock === "yes") {
@@ -16,35 +16,37 @@ export default ({ product, outOfStock }) => {
 		}
 	}, [outOfStock]);
 
-	return (
-		<tr className={stockText}>
-			<td>
-				{product.name} ({product.size})
-				{outOfStock === "yes" ? " (Out Of Stock)" : ""}
-			</td>
-			<td>
+	let quantity;
+
+	if (outOfStock === "yes") {
+		quantity = product.quantity;
+	} else {
+		quantity = (
+			<>
 				<i
 					onClick={() =>
-						decrease(
-							product.id,
-							product.variant_id,
-							product.size
-						)
+						decrease(product.id, product.variant_id, product.size)
 					}
 					className="fa fa-minus"
 				></i>{" "}
 				{product.quantity}{" "}
 				<i
 					onClick={() =>
-						increase(
-							product.id,
-							product.variant_id,
-							product.size
-						)
+						increase(product.id, product.variant_id, product.size)
 					}
 					className="fa fa-plus"
 				></i>
+			</>
+		);
+	}
+
+	return (
+		<tr className={stockText}>
+			<td>
+				{product.name} ({product.size})
+				{outOfStock === "yes" ? " (Out Of Stock)" : ""}
 			</td>
+			<td>{quantity}</td>
 			<td>{product.price}</td>
 			<td>
 				<button
