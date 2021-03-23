b4bdf1eaa5fbe08e20f71c156e2eafd510b5ece5
jitesh goel <jitesh.goel@imarkinfotech.com>
2021-03-23 19:22:11 +0530

    issue#1 remove console.log

--- frontend/src/util/Axios.js
+++ frontend/src/util/Axios.js
@@ -7,10 +7,10 @@ axios.defaults.headers = setHeaders()
 
 axios.interceptors.response.use(response => {
     if(response.status !== 401) return response;
-    axios.post('/auth/token/refresh/', {
-        "refresh_token": refreshToken,
+    axios.post('/api/core/auth/token/refresh/', {
+        "refresh": refreshToken,
     }).then(response => {
-        saveTokens(response.data["access_token"], response.data["refresh_token"]);
+        saveTokens(response.data.access, refreshToken);
         axios.defaults.headers = setHeaders()
         window.location.reload()
     }).catch(error => {
@@ -23,7 +23,6 @@ axios.interceptors.response.use(response => {
         axios.post('/api/core/auth/token/refresh/', {
             "refresh": refreshToken,
         }).then(response => {
-            console.log(response)
             saveTokens(response.data.access, refreshToken);
             axios.defaults.headers = setHeaders()
             window.location.reload()
