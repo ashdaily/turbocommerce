200713eaabfd39e65d1e4c11d1c836e5b7a056ab
jitesh goel <jitesh.goel@imarkinfotech.com>
2021-03-31 01:10:42 +0530

    issue#38#39 Add Social Login from Google and Facebook

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,76 +1,131 @@
 import React, { useState } from "react";
-import { Row, Col, Form, Button, Card } from 'react-bootstrap';
+import { Row, Col, Form, Button, Card } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 import { saveTokens } from "../util/Auth";
 import axios from "../util/Axios";
+import SocialButton from "../components/SocialButton";
+import google from '../google.svg'
+import '../css/login.scss'
 
+export default () => {
+	const [email, setEmail] = useState(null);
+	const [password, setPassword] = useState(null);
+	const [redirect, setRedirect] = useState(false);
+	const [validated, setValidated] = useState(false);
 
-export default ()=>{
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [redirect, setRedirect] = useState(false);
-    const [validated, setValidated] = useState(false);
+	const handleSocialLogin = (user) => {
+		console.log(user);
+	};
 
-    function handleForm(e){
-        const form = e.currentTarget;
-        e.preventDefault();
-        if (form.checkValidity() === false) {
-            e.stopPropagation();
-            setValidated(true);
-            return
-        }
+	const handleSocialLoginFailure = (err) => {
+		console.error(err);
+	};
 
-        const payload = {
-            "username": email,
-            "password": password
-        }
-        axios.post("api/core/auth/token/",  payload)
-        .then(response => {
-            if(response.status === 200){
-                const accessToken = response.data.access;
-                const refreshToken = response.data.refresh;
-                saveTokens(accessToken, refreshToken);
-                setRedirect(true);
-                // FIXME: confirm is there is a better way to do this
-                window.location.reload()
-            }
-        })
-        .catch(error => {
-            console.error(error)
-        })
-    }
+	function handleForm(e) {
+		const form = e.currentTarget;
+		e.preventDefault();
+		if (form.checkValidity() === false) {
+			e.stopPropagation();
+			setValidated(true);
+			return;
+		}
 
-    const form = (
-        <Row>
-            <Col md={{span: 6, offset:3}}>
-                <Card title="Login" className="p-3 mt-5">
-                    <Form noValidate validated={validated} onSubmit={handleForm}>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control required type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
-                            <Form.Control.Feedback type="invalid">
-                            Please choose a email.
-                            </Form.Control.Feedback>
-                        </Form.Group>
+		const payload = {
+			username: email,
+			password: password,
+		};
+		axios
+			.post("api/core/auth/token/", payload)
+			.then((response) => {
+				if (response.status === 200) {
+					const accessToken = response.data.access;
+					const refreshToken = response.data.refresh;
+					saveTokens(accessToken, refreshToken);
+					setRedirect(true);
+					// FIXME: confirm is there is a better way to do this
+					window.location.reload();
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
-                            <Form.Control.Feedback type="invalid">
-                            Please choose a password.
-                            </Form.Control.Feedback>
-                        </Form.Group>
+	const form = (
+		<Row>
+			<Col md={{ span: 6, offset: 3 }}>
+				<Card title="Login" className="p-3 mt-5 text-center">
+					<Form
+						noValidate
+						validated={validated}
+						onSubmit={handleForm}
+					>
+						<Form.Group controlId="formBasicEmail">
+							<Form.Label>Email address</Form.Label>
+							<Form.Control
+								required
+								type="email"
+								placeholder="Enter email"
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<Form.Control.Feedback type="invalid">
+								Please choose a email.
+							</Form.Control.Feedback>
+						</Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Login
-                        </Button>
-                    </Form>
-                </Card>
-            </Col>
-        </Row>
-    )
+						<Form.Group controlId="formBasicPassword">
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								required
+								type="password"
+								placeholder="Password"
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+							<Form.Control.Feedback type="invalid">
+								Please choose a password.
+							</Form.Control.Feedback>
+						</Form.Group>
 
-    if(redirect) return <Redirect to="/" />;
+						<Button variant="primary" type="submit" block>
+							Login
+						</Button>
+					</Form>
+					<center
+						style={{
+							borderBottom: "1px solid #ced4da",
+							lineHeight: "0.1em",
+                            margin: '15px 0'
+						}}
+					>
+						<span style={{ background: "#fff", padding: '0 5px' }}>OR</span>
+					</center>
+					<SocialButton
+						className="gmail-btn mt-2"
+						provider="google"
+						appId="427421314489-kd63pc8k47enqeuauctb5dtu3c6hhi6f.apps.googleusercontent.com"
+						onLoginSuccess={handleSocialLogin}
+						onLoginFailure={handleSocialLoginFailure}
+					>
+						<img src={google} /> Login with Google
+					</SocialButton>
+					<SocialButton
+						variant="primary"
+                        className="facebook-btn mt-2"
+						style={{ "background-color": "#3c5898" }}
+						provider="facebook"
+						appId="1762595890586028"
+						onLoginSuccess={handleSocialLogin}
+						onLoginFailure={handleSocialLoginFailure}
+					>
+						<i className="fa fa-facebook-square"></i> Login with
+						Facebook
+					</SocialButton>
+				</Card>
+			</Col>
+		</Row>
+	);
 
-    return form;
-}
+	if (redirect) return <Redirect to="/" />;
+
+	return form;
+};
