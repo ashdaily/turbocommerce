41f58b6279ddbc22234f5e80cc6e11c2d4998c0c
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-04-03 08:25:08 +0900

    fix

--- travis/make_env_file.sh
+++ travis/make_env_file.sh
@@ -1,21 +1,19 @@
 cd backend/
 
 # Using > to overwrite
-echo "PROJECT_NAME=${PROJECT_NAME}\n" > .env
+echo "PROJECT_NAME=${PROJECT_NAME}" > .env
 
 # Using >> to append
-echo "SECRET_KEY=${SECRET_KEY}\n" >> .env
-echo "REACT_APP_URL=${REACT_APP_URL}\n" >> .env
-echo "DEBUG=${DEBUG}\n" >> .env
-echo "AWS_S3_REGION_NAME=${AWS_S3_REGION_NAME}\n" >> .env
-echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\n" >> .env
-echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}\n" >> .env
-echo "REST_FRAMEWORK_PAGE_SIZE=${REST_FRAMEWORK_PAGE_SIZE}\n" >> .env
-echo "REST_FRAMEWORK_MAX_PAGE_SIZE=${REST_FRAMEWORK_MAX_PAGE_SIZE}\n" >> .env
-echo "POSTGRES_NAME=${POSTGRES_NAME}\n" >> .env
-echo "POSTGRES_USER=${POSTGRES_USER}\n" >> .env
-echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}\n" >> .env
-echo "POSTGRES_HOST=${POSTGRES_HOST}\n" >> .env
-echo "POSTGRES_PORT=${POSTGRES_PORT}\n" >> .env
-
-cd ..
+echo "SECRET_KEY=${SECRET_KEY}" >> .env
+echo "REACT_APP_URL=${REACT_APP_URL}" >> .env
+echo "DEBUG=${DEBUG}" >> .env
+echo "AWS_S3_REGION_NAME=${AWS_S3_REGION_NAME}" >> .env
+echo "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> .env
+echo "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> .env
+echo "REST_FRAMEWORK_PAGE_SIZE=${REST_FRAMEWORK_PAGE_SIZE}" >> .env
+echo "REST_FRAMEWORK_MAX_PAGE_SIZE=${REST_FRAMEWORK_MAX_PAGE_SIZE}" >> .env
+echo "POSTGRES_NAME=${POSTGRES_NAME}" >> .env
+echo "POSTGRES_USER=${POSTGRES_USER}" >> .env
+echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> .env
+echo "POSTGRES_HOST=${POSTGRES_HOST}" >> .env
+echo "POSTGRES_PORT=${POSTGRES_PORT}" >> .env
