9a2cd6bf71ea5aee4726d85806171cc34d90d152
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-04-14 00:53:57 +0900

    issue#70: admin register wishlist

--- backend/src/customer/admin.py
+++ backend/src/customer/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from customer.models import CustomerShippingAddress, CustomerCreditCard
+from customer.models import (
+    CustomerShippingAddress,
+    CustomerCreditCard,
+    CustomerWishlist,
+)
 
 
+admin.site.register(CustomerWishlist)
 admin.site.register(CustomerShippingAddress)
 admin.site.register(CustomerCreditCard)
