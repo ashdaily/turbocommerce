3d4234c8edf0f543e1a1b70e29418db552ea1d8e
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-04-17 20:48:54 +0900

    issue#63: added timestamp to store, added StoreCustomer model

--- backend/src/store/models.py
+++ backend/src/store/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from core.models import User
+from core.models import User, Timestamp
 
 
 def path_for_store_logo(instance, filename):
     return f"{instance.store_name}/logo/{filename}"
 
 
-class Store(models.Model):
+class Store(Timestamp):
     store_owner = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -20,3 +20,18 @@ class Store(models.Model):
 
     def __str__(self):
         return self.store_name
+
+
+class StoreCustomer(Timestamp):
+    store = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+        related_name="customer_stores",
+        related_query_name="customer_store",
+    )
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="store_customers",
+        related_query_name="store_customer",
+    )
