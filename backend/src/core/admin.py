c227282a899cbfcb17836183826c87fe9f20a7e9
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-04-19 06:32:05 +0900

    issue#63: remove username User model, point to email

--- backend/src/core/admin.py
+++ backend/src/core/admin.py
@@ -4,4 +4,8 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
+from django.utils.translation import gettext_lazy as _
 
 
 class Timestamp(models.Model):
@@ -19,6 +20,13 @@ class User(AbstractUser):
     CUSTOMER = "CUSTOMER"
     ADMIN = "ADMIN"
     USER_TYPES = ((CUSTOMER, CUSTOMER), (ADMIN, ADMIN))
+
+    # Get rid of username, and point it to email instead
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     user_type = models.CharField(max_length=20, choices=USER_TYPES)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
