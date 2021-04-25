f155c8e179f177a4beb61f8a5b801898086219d1
jitesh goel <jitesh.goel@imarkinfotech.com>
2021-04-26 02:29:04 +0530

    issue#69 signup not displaying validation errors

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,80 +1,102 @@
 import React, { useState } from "react";
-import { Row, Col, Form, Button, Card } from 'react-bootstrap';
+import { Row, Col, Form, Button, Card } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 
 import axios from "../util/Axios";
 
+export default () => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [agree, setAgree] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState(null);
 
-export default ()=>{
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [agree, setAgree] = useState(false);
-    const [redirect, setRedirect] = useState(false);
-    const [validated, setValidated] = useState(false);
-
-    function handleForm(e){
-        const form = e.currentTarget;
-        e.preventDefault();
-        if (form.checkValidity() === false) {
-            e.stopPropagation();
-            setValidated(true);
-            return
-        }
-
-        const payload = {
-            "username": email,
-            "password": password
-        }
-        axios.post("api/core/customer/signup/",  payload)
-        .then(response => {
-            if(response.status === 200){
-                setRedirect(true)
-            }
-        })
-        .catch(error => {
-            console.error(error)
-
-        })
+  function handleForm(e) {
+    setError(null);
+    const form = e.currentTarget;
+    e.preventDefault();
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      setValidated(true);
+      return;
     }
 
-    const form = (
-        <Row>
-            <Col md={{span: 6, offset:3}}>
-                <Card title="Login" className="p-3 mt-5">
-                    <Form noValidate validated={validated} onSubmit={handleForm}>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control required type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                            <Form.Control.Feedback type="invalid">
-                            Please choose a email.
-                            </Form.Control.Feedback>
-                        </Form.Group>
+    const payload = {
+      username: email,
+      password: password,
+    };
+    axios
+      .post("api/core/customer/signup/", payload)
+      .then((response) => {
+        if (response.status === 200) {
+          setRedirect(true);
+        }
+      })
+      .catch((error) => {
+        setError(error.response.data);
+      });
+  }
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
-                            <Form.Control.Feedback type="invalid">
-                            Please choose a password.
-                            </Form.Control.Feedback>
-                        </Form.Group>
+  const form = (
+    <Row>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Card title="Login" className="p-3 mt-5">
+          <Form noValidate validated={validated} onSubmit={handleForm}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                required
+                type="email"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+              <Form.Control.Feedback type="invalid">
+                Please choose a email.
+              </Form.Control.Feedback>
+              {error ? (
+                <Form.Text className="text-danger">
+                  {error.username[0]}
+                </Form.Text>
+              ) : (
+                ""
+              )}
+            </Form.Group>
 
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="I agree with all terms and conditions." onChange={e=>setAgree(e.target.checked)}/>
-                        </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please choose a password.
+              </Form.Control.Feedback>
+            </Form.Group>
 
-                        <Button variant="primary" type="submit" disabled={!agree}>
-                            Sign up
-                        </Button>
-                    </Form>
-                </Card>
-            </Col>
-        </Row>
-    )
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="I agree with all terms and conditions."
+                onChange={(e) => setAgree(e.target.checked)}
+              />
+            </Form.Group>
 
-    if(redirect) return <Redirect to="/" />;
+            <Button variant="primary" type="submit" disabled={!agree}>
+              Sign up
+            </Button>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
 
-    return form;
-}
+  if (redirect) return <Redirect to="/" />;
+
+  return form;
+};
