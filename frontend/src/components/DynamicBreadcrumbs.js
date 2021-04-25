9cf973ca84e84f28782c2a1acf9184e571bd32e6
jitesh goel <jitesh.goel@imarkinfotech.com>
2021-04-26 03:09:36 +0530

    issue#72 fix breadcrumb, remove hyphens and Capitalise first letter of first word

--- frontend/src/components/DynamicBreadcrumbs.js
+++ frontend/src/components/DynamicBreadcrumbs.js
@@ -3,6 +3,10 @@ import Breadcrumbs from "react-router-dynamic-breadcrumbs";
 
 const routes = {
   "/": "Home",
+  "/:grandParentCategory": (url, match) => `${match[':grandParentCategory'].replace('-', ' ')[0].toUpperCase()}${match[':grandParentCategory'].replace('-', ' ').slice(1)}`,
+  "/:grandParentCategory/:parentCategory": (url, match) => `${match[':parentCategory'].replace('-', ' ')[0].toUpperCase()}${match[':parentCategory'].replace('-', ' ').slice(1)}`,
+  "/:grandParentCategory/:parentCategory/:childCategory": (url, match) => `${match[':childCategory'].replace('-', ' ')[0].toUpperCase()}${match[':childCategory'].replace('-', ' ').slice(1)}`,
+  "/:grandParentCategory/:parentCategory/:childCategory/:slug": (url, match) => `${match[':slug'].replace('-', ' ')[0].toUpperCase()}${match[':slug'].replace('-', ' ').slice(1)}`,
   "/:id": ":id",
 };
 
