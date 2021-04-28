0a06ce0ec73e907ac3bd4b346b7612a426f1b91f
Jerry <jitesh.goel26@gmail.com>
2021-04-28 20:10:00 +0530

    issue#72 fix breadcrumb, remove hyphens and Capitalise first letter o… (#83)
    * issue#72 fix breadcrumb, remove hyphens and Capitalise first letter of first word
    
    * issue#72 resolve the duplicate enteries to make a helper
    
    Co-authored-by: jitesh goel <jitesh.goel@imarkinfotech.com>

--- frontend/src/components/DynamicBreadcrumbs.js
+++ frontend/src/components/DynamicBreadcrumbs.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Breadcrumbs from "react-router-dynamic-breadcrumbs";
 
+import { removeHyphenAndCapitalize } from '../util/Helpers'
+
 const routes = {
   "/": "Home",
+  "/:grandParentCategory": (url, match) => removeHyphenAndCapitalize(match[':grandParentCategory']),
+  "/:grandParentCategory/:parentCategory": (url, match) => removeHyphenAndCapitalize(match[':parentCategory']),
+  "/:grandParentCategory/:parentCategory/:childCategory": (url, match) => removeHyphenAndCapitalize(match[':childCategory']),
+  "/:grandParentCategory/:parentCategory/:childCategory/:slug": (url, match) => removeHyphenAndCapitalize(match[':slug']),
   "/:id": ":id",
 };
 
