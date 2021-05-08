aada9cc28404c9af24e411612506eb54483657ad
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-05-09 00:42:53 +0900

    import bug fix

--- backend/src/store/views.py
+++ backend/src/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
