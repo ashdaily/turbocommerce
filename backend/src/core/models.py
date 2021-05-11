c2eca052cc3f95e41d4d2716e04200c01844c438
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-05-12 01:00:25 +0900

    backend: add modelmanager for custom django-user

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -16,6 +16,44 @@ class Timestamp(models.Model):
         ordering = ("-created",)
 
 
+class CustomUserManager(UserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     CUSTOMER = "CUSTOMER"
     ADMIN = "ADMIN"
@@ -24,13 +62,14 @@ class User(AbstractUser):
     # Getting rid of django inbuilt username, and pointing it to email instead
     username = None
     email = models.EmailField(_("email address"), unique=True)
-    EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     user_type = models.CharField(max_length=20, choices=USER_TYPES)
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     address_pincode = models.IntegerField(null=True, blank=True)
+
+    objects = CustomUserManager()
 
     @property
     def is_customer(self):
