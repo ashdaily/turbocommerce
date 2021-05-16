9d23c116af4d2850d3aaedc43b1b96b31917b57f
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-05-17 04:52:01 +0900

    backend: fixed django admin error

--- backend/src/core/admin.py
+++ backend/src/core/admin.py
@@ -1,11 +1,46 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from core.models import User
 
 
-class CustomUserAdmin(UserAdmin):
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "date_joined",
+    )
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                    "user_type",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "phone_number",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
-
-
-admin.site.register(User, CustomUserAdmin)
