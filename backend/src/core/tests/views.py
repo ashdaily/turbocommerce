9c72b0b92195073086097cda68ce695f622bd308
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-05-18 22:29:26 +0900

    backend: add a test for customer login

--- backend/src/core/tests/views.py
+++ backend/src/core/tests/views.py
@@ -18,15 +18,33 @@ class TestCustomerSignup(TestCaseBase):
             "password": "ashish123",
         }
 
-        r = self.client.post("/api/core/customer/signup/", payload)
-        self.assertEqual(r.status_code, 200)
+        r = self.client.post(self.url, payload)
+        self.assertEqual(r.status_code, 201)
 
     def test_raise_validation_error_when_email_not_valid(self):
         payload = {
             "email": "ash",
             "password": "ashish123",
         }
-        r = self.client.post("/api/core/customer/signup/", payload)
+        r = self.client.post(self.url, payload)
         self.assertEqual(r.status_code, 400)
         r = self.deserialize(r)
         self.assertEqual(r["email"], ["Enter a valid email address."])
+
+
+class TestCustomerLogin(TestCaseBase):
+    fixtures = [
+        "core/tests/fixtures/users.json",
+    ]
+    url = reverse("token_obtain_pair")
+
+    def test_login_for_customer(self):
+        payload = {
+            "email": "ashish@gmail.com",
+            "password": "ashish123",
+        }
+        r = self.client.post(reverse("customer-signup"), payload)
+        self.assertEqual(r.status_code, 201)
+
+        r = self.client.post(self.url, payload)
+        self.assertEqual(r.status_code, 200)
