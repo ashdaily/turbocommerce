78517f1c05cf3213307d31289cd364978c68dda4
charanjeet singh <charanjit.singh25@gmail.com>
2021-06-09 22:50:47 +0530

    #13 cart item added

--- frontend/src/pages/Cart/Cart.test.js
+++ frontend/src/pages/Cart/Cart.test.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import EmptyCart from "./EmptyCart";
-import { shallow, mount } from "enzyme";
+import {shallow, mount} from "enzyme";
 import Cart from './Cart';
-import ShopContextProvider from "../../context/ShopContext";
-import { BrowserRouter as Router } from 'react-router-dom';
+import ShopContextProvider, {ShopContext} from "../../context/ShopContext";
+import {BrowserRouter as Router} from 'react-router-dom';
+import CartItem from "./CartItem";
+
+const cartItems = [{
+    "id": 4,
+    "variant_id": 6,
+    "name": "sleeve product",
+    "price": 1200,
+    "in_stock": true,
+    "size": "m",
+    "color": "#FF0F36",
+    "image": "/static/media/logo.eb741475.jpg",
+    "brand": "tommy",
+    "quantity": 1,
+    "product_url": "/men/tshirts/sleeves/sleeve-product"
+}]
 
 describe('<Cart />', () => {
-    it('renders cart components', () => {
+    it('renders cart components without any item', () => {
+        const wrapper = mount(<Router><ShopContextProvider><Cart/></ShopContextProvider></Router>);
+        expect(wrapper.contains(EmptyCart)).toBe(true);
+
+    });
+
+    it('renders cart components with cart items', () => {
         // const contextValue = { a: '1' };
         // jest.spyOn(ShopContext, 'useShopContext').mockImplementation(() => contextValue);
-        const wrapper = mount(<Router><ShopContextProvider><Cart/></ShopContextProvider></Router>);
 
+        const wrapper = mount(<Router>
+            <ShopContext.Provider
+                value={{cartItems: cartItems, storeInfo: {default_currency: 'RS'}}}>
+                <Cart/>
+            </ShopContext.Provider>
+        </Router>);
         // expect((wrapper)).toMatchSnapshot();
-
-        expect(wrapper.contains(EmptyCart)).toBe(true);
+        expect(wrapper.contains(CartItem)).toBe(true);
 
     });
 });
