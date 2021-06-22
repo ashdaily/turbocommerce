aeaa855f9a1b785f634922b0433fd3eda1be22c9
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-06-23 02:36:15 +0900

    customer shipping address model changes

--- backend/src/customer/models.py
+++ backend/src/customer/models.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from products.models import Product
 from core.models import Timestamp, User
@@ -16,15 +18,29 @@ class CustomerWishlist(Timestamp):
 
 
 class CustomerShippingAddress(Timestamp):
+    ADDRESS_TYPE_HOME = "HOME"
+    ADDRESS_TYPE_OFFICE = "OFFICE"
+
+    ADDRESS_TYPE_CHOICES = (
+        (ADDRESS_TYPE_HOME, ADDRESS_TYPE_HOME),
+        (ADDRESS_TYPE_OFFICE, ADDRESS_TYPE_OFFICE),
+    )
+
     customer = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="customer_shipping_addresses",
         related_query_name="customer_shipping_address",
     )
-    address_street_line_1 = models.CharField(max_length=255, null=True, blank=True)
-    address_street_line_2 = models.CharField(max_length=255, null=True, blank=True)
+    customer_name = models.CharField(max_length=200)
+    address = models.TextField(max_length=500, null=True, blank=True)
     city = models.CharField(max_length=255)
     province = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=20)
+    country_code_primary_phone_number = models.CharField(max_length=5)
+    primary_phone_number = models.CharField(max_length=20)
+    country_code_alternate_phone_number = models.CharField(max_length=5)
+    alternate_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    address_type = models.CharField(choices=ADDRESS_TYPE_CHOICES, max_length=20)
+    default_address = models.BooleanField(default=True)
