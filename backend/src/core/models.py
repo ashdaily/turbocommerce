6ad147a3f508f32aa918dec196a8e47cd5bbe303
Ashish Singh Vardhan <ashishsinghbardhan@gmail.com>
2021-06-23 03:22:15 +0900

    remove unused imports

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from django.utils.translation import gettext_lazy as _
 
